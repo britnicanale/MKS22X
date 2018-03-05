b5cbf61fe7e9761cb032f5dbe3d4e490ee5683a2
Britni <bcanale@stuy.edu>
2018-03-04 22:50:22 -0500

    Finished maze doing final testing

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -1,5 +1,6 @@
 public class KnightBoard{
     private int[][] board;
+    private int[][] posMoves;
     public KnightBoard(int row, int col){
 
 	if(row < 0 || col < 0){
@@ -21,7 +22,6 @@ public class KnightBoard{
  
     private boolean solveHelp(int startingRow, int startingCol, int level){
 	if(level > board.length * board[0].length){
-	    //board[startingRow][startingCol] = level;
 	    return true;
 	}
 	if((startingRow < 0 || startingRow >= board.length)|| (startingCol < 0 || startingCol >= board[0].length)){
@@ -70,6 +70,85 @@ public class KnightBoard{
 
 
 
+    /*private void fillMoves(){
+	posMoves = new int[board.length][board[0].length];
+	for(int i = 0; i < posMoves.length; i++){
+	    for(int j = 0; j < posMoves[0].length; j++){
+		posMoves[i][j] = 0;
+		if(i - 2 >= 0 && j - 1 >= 0){
+		    posMoves[i][j] = posMoves[i][j] + 1;
+		}
+		if(i - 1 >= 0 && j - 2 >= 0){
+                    posMoves[i][j] = posMoves[i][j]+ 1;
+		}
+		if(i + 1 < posMoves.length && j - 2 >= 0){
+                    posMoves[i][j] = posMoves[i][j]+ 1;
+		}
+		if(i + 2 < posMoves.length  &&j - 1 >= 0){
+                    posMoves[i][j] = posMoves[i][j]+ 1;
+		}
+		if(i + 2 < posMoves.length && j + 1 < posMoves[0].length){
+                    posMoves[i][j] = posMoves[i][j]+ 1;
+		}
+		if(i + 1 < posMoves.length && j + 2 < posMoves[0].length){
+                    posMoves[i][j] = posMoves[i][j]+ 1;
+		}
+		if(i - 1 >= 0 && j + 2 < posMoves[0].length){
+                    posMoves[i][j] = posMoves[i][j]+ 1;
+		}
+		if(i - 2 >= 0 && j + 1 < posMoves[0].length){
+                    posMoves[i][j] = posMoves[i][j]+ 1;
+		}
+	    }
+	}
+    }
+    public boolean solveFast(int startingRow, int startingCol){
+	if(startingRow < 0 || startingCol < 0){
+            throw new IllegalArgumentException();
+        }
+	return solveFHelp(startingRow, startingCol, 1);
+    }
+    private boolean solveFHelp(int row, int col, int level){
+	if(level > board.length * board[0].length){
+            return true;
+        }
+        if((row < 0 || row >= board.length)|| (col < 0 || col >= board[0].length)){
+            return false;
+        }
+        if(board[row][col] != 0){
+            return false;
+        }
+
+	board[row][col] = level;
+	changeMoves(row,col);
+
+    }
+    private void changeMoves(int row, int col){
+	if(i - 2 >= 0 && j - 1 >= 0){
+	    posMoves[i][j] = posMoves[i][j] - 1;
+	}
+	if(i - 1 >= 0 && j - 2 >= 0){
+	    posMoves[i][j] = posMoves[i][j] - 1;
+	}
+	if(i + 1 < posMoves.length && j - 2 >= 0){
+	    posMoves[i][j] = posMoves[i][j] - 1;
+	}
+	if(i + 2 < posMoves.length  &&j - 1 >= 0){
+	    posMoves[i][j] = posMoves[i][j]- 1;
+	}
+	if(i + 2 < posMoves.length && j + 1 < posMoves[0].length){
+	    posMoves[i][j] = posMoves[i][j]- 1;
+	}
+	if(i + 1 < posMoves.length && j + 2 < posMoves[0].length){
+	    posMoves[i][j] = posMoves[i][j]- 1;
+	}
+	if(i - 1 >= 0 && j + 2 < posMoves[0].length){
+	    posMoves[i][j] = posMoves[i][j]- 1;
+	}
+	if(i - 2 >= 0 && j + 1 < posMoves[0].length){
+	    posMoves[i][j] = posMoves[i][j]- 1;
+	}
+	}*/
     public String toString(){
 	String str = "";
 	for(int i  = 0; i < board.length; i++){
@@ -84,10 +163,27 @@ public class KnightBoard{
 	}
 	return str;
     }
+    /*
+    public String prntMoves(){
+        String str = "";
+        for(int i  = 0; i < posMoves.length; i++){
+            for(int j  = 0; j < posMoves[i].length; j++){
+                if(board[i][j] <  10){
+                    str+= " " + posMoves [i][j] + " ";
+                }else{
+                    str+=  posMoves[i][j] + " ";
+                }
+            }
+            str += "\n";
+        }
+        return str;
+	}*/
+
     public static void main(String[] args){
 	KnightBoard k = new KnightBoard(7, 7);
-	System.out.println(k.countSolutions(0,0));
+	//System.out.println(k.countSolutions(0,0));
+	k.fillMoves();
 	//System.out.println(k.solve(0, 0));
-	System.out.println(k);
+	System.out.println(k.prntMoves());
     }
 }
--- 04Maze/Maze.java
+++ 04Maze/Maze.java
@@ -8,10 +8,24 @@ public class Maze{
     public Maze(String filename) throws FileNotFoundException{
 	int numE = 0;
 	int numS = 0;
-	File f = new File(fileName);
+   
+	File f = new File(filename);
 	Scanner in = new Scanner(f);
-	for(int j = 0; in.hasNext(); j++){
-	    String line = in.next();
+	int numRows = 0;
+	int numCols = 0;
+	while(in.hasNextLine()){
+	    numRows++;
+	    in.nextLine();
+	}
+	Scanner l = new Scanner(f);
+	String s = l.nextLine();
+	for(int i = 0; i < s.length(); i++){
+	    numCols++;
+	}
+	Scanner m = new Scanner(f);
+	maze = new char[numRows][numCols];
+	for(int j = 0; m.hasNextLine(); j++){
+	    String line = m.nextLine(); 
 	    for(int i = 0; i < line.length(); i++){
 		if(line.charAt(i) == 'S'){
 		    numS ++;
@@ -19,13 +33,12 @@ public class Maze{
 		if(line.charAt(i) == 'E'){
 		    numE ++;
 		}
-		maze[j][i] == line.charAt(i);
+		maze[j][i] = line.charAt(i);
 	    }
 	}
 	if(numS != 1 || numE != 1){
 	    throw new IllegalStateException();
 	}
-
     }
     
     private void wait(int millis){
@@ -44,22 +57,61 @@ public class Maze{
         System.out.println("\033[2J\033[1;1H");
     }
 
-
    
     public int solve(){
+	for(int i  = 0; i < maze.length; i++){
+            for(int j  = 0; j < maze[i].length; j++){
+		if(maze[i][j] == 'S'){
+		    maze[i][j] = ' ';
+		    return solve(i, j, 0);
+		}
+	    }
+	}
+	return -1;
     }
 
-    private int solve(int row, int col){
-
+    private int solve(int row, int col, int numSteps){
+	if(maze[row][col] == 'E'){
+	    return numSteps;
+	}
+	if(maze[row][col] != ' '){
+	    return -1;
+	}
+	maze[row][col] = '@';
+	numSteps++;
         if(animate){
             clearTerminal();
             System.out.println(this);
-            wait(20);
+            wait(200);
         }
-
-        return -1;
+	if((solve(row, col + 1, numSteps) == -1 && solve(row + 1, col, numSteps) == -1) && (solve(row, col - 1, numSteps) == -1 && solve(row - 1, col, numSteps) == -1)){
+	    maze[row][col] = '.';
+	    numSteps--;
+	    return -1;
+	}
+	return 0;
     }
+
+    public String toString(){
+        String str = "";
+        for(int i  = 0; i < maze.length; i++){
+            for(int j  = 0; j < maze[i].length; j++){
+                str += maze[i][j];
+            }
+            str += "\n";
+        }
+        return str;
+    }
+
     public static void main(String[] args){
-	Maze m = new Maze(data1.dat);
+	try{
+	    Maze m = new Maze("data5.dat");
+	    m.setAnimate(true);
+	    m.solve();
+	    System.out.println(m);
+	}catch(FileNotFoundException e){
+	    System.out.println("WHAT");
+	    System.exit(0);
+	}
     }
 }
