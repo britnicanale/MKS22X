70970eb04f114154f3214816aa60762f2ac49ceb
Britni <bcanale@stuy.edu>
2018-04-30 18:26:15 -0400

    Fixed errors in heap

--- 12MyHeap/MyHeap.java
+++ 12MyHeap/MyHeap.java
@@ -2,24 +2,28 @@ import java.util.Arrays;
 import java.util.Random;
 
 
-public class MyHeap{
+public class MyHeap<T extends Comparable<T>>{
 
-    private String[] data;
+    private T[] data;
     private int size;
     private boolean isMax;
 
+    @SuppressWarnings("unchecked")
     public MyHeap(){
-	data = new String[10];
+	data = (T[]) new Comparable[10];
 	size = 0;
 	isMax = true;
     }
 
+
+    @SuppressWarnings("unchecked")
     public MyHeap(boolean max){
-	data = new String[10];
+	data = (T[]) new Comparable[10];
         size = 0;
         isMax = max;
     }
-    public void add(String s){
+
+    public void add(T s){
 	if(size == data.length){
 	    resize();
 	}
@@ -27,22 +31,23 @@ public class MyHeap{
 	size++;
 	pushup(size - 1);
     }
-    public String remove(){
-	String top = data[0];
+    public T remove(){
+	T top = data[0];
 	data[0] = data[size-1];
 	size--;
 	pushdown(0);
 	return top;
     }
-    public String peek(){
+    public T peek(){
 	return data[0];
     }
     public int size(){
 	return size;
     }
 
+    @SuppressWarnings("unchecked")
     private void resize(){
-	String[] n = new String[data.length*2];
+	T[] n = (T[]) new Comparable[data.length*2];
 	for(int i = 0; i < size; i++){
 	    n[i] = data[i];
 	}
@@ -50,12 +55,12 @@ public class MyHeap{
     }
     private void pushup(int index){
 	if(isMax && data[index].compareTo(data[(index - 1) / 2]) > 0){
-	    String temp = data[(index -1) / 2];
+	    T temp = data[(index -1) / 2];
 	    data[(index -1) / 2] = data[index];
 	    data[index] = temp;
 	    pushup((index -1) / 2);
 	}else if(!isMax && data[index].compareTo(data[(index -1) / 2]) < 0){
-	    String temp = data[(index -1) / 2];
+	    T temp = data[(index -1) / 2];
 	    data[(index -1) / 2] = data[index];
 	    data[index] = temp;
 	    pushup((index -1) / 2);
@@ -64,41 +69,82 @@ public class MyHeap{
 
 
     private void pushdown(int index){
-	if(2 * index + 1 < size){
-	    if(isMax && data[2 * index + 2].compareTo(data[index]) > 0 || data[2 * index + 1].compareTo(data[index]) > 0){
-		if(2*index + 2 < size){
-		    if(data[2 * index + 2].compareTo(data[2*index + 1]) > 0){
-			String temp = data[2 * index + 2];
-			data[2 * index + 2] = data[index];
-			data[index] = temp;
-			pushdown(2 * index + 2);
-		    }else {
-			String temp = data[2 * index + 1];
-			data[2 * index + 1] = data[index];
-			data[index] = temp;
-			pushdown(2 * index + 1);
-		    }
-		}else if(2*index + 2 < size){
-		    String temp = data[2 * index + 1];
+	if(2*index+2 < size){
+	    if(isMax && (data[2 * index + 2].compareTo(data[index]) > 0 || data[2 * index + 1].compareTo(data[index]) > 0)){
+		if(data[2 * index + 2].compareTo(data[2*index + 1]) > 0){
+		    T temp = data[2 * index + 2];
+		    data[2 * index + 2] = data[index];
+		    data[index] = temp;
+		    pushdown(2 * index + 2);
+		}else {
+		    T temp = data[2 * index + 1];
 		    data[2 * index + 1] = data[index];
 		    data[index] = temp;
 		    pushdown(2 * index + 1);
 		}
-	    }else if(!isMax && data[2 * index + 2].compareTo(data[index]) < 0 || data[2 * index + 1].compareTo(data[index]) < 0){
+	    }else if(!isMax && (data[2 * index + 2].compareTo(data[index]) < 0 || data[2 * index + 1].compareTo(data[index]) < 0)){
+		if(data[2 * index + 2].compareTo(data[2*index + 1]) < 0){
+		    T temp = data[2 * index + 2];
+		    data[2 * index + 2] = data[index];
+		    data[index] = temp;
+		    pushdown(2 * index + 2);
+		}else {
+		    T temp = data[2 * index + 1];
+		    data[2 * index + 1] = data[index];
+		    data[index] = temp;
+		    pushdown(2 * index + 1);
+		}
+	    }
+	}else if(2* index + 1 < size){
+	    if(isMax && data[2 * index + 1].compareTo(data[index]) > 0){
+		T temp = data[2 * index + 1];
+		data[2 * index + 1] = data[index];
+		data[index] = temp;
+		pushdown(2 * index + 1);
+	    }else if(!isMax && data[2 * index + 1].compareTo(data[index]) < 0){
+		T temp = data[2 * index + 1];
+		data[2 * index + 1] = data[index];
+		data[index] = temp;
+		pushdown(2 * index + 1);
+	    }
+	}
+    }
+    /*
+	if(2 * index + 1 < size){
+	    if(isMax && (data[2 * index + 2].compareTo(data[index]) > 0 || data[2 * index + 1].compareTo(data[index]) > 0)){
 		if(2*index + 2 < size){
-		    if(data[2 * index + 2].compareTo(data[2*index + 1]) < 0){
-			String temp = data[2 * index + 2];
+		    if(data[2 * index + 2].compareTo(data[2*index + 1]) > 0){
+			T temp = data[2 * index + 2];
 			data[2 * index + 2] = data[index];
 			data[index] = temp;
 			pushdown(2 * index + 2);
 		    }else {
-			String temp = data[2 * index + 1];
+			T temp = data[2 * index + 1];
 			data[2 * index + 1] = data[index];
 			data[index] = temp;
 			pushdown(2 * index + 1);
 		    }
 		}else if(2*index + 2 < size){
-		    String temp = data[2 * index + 1];
+		    T temp = data[2 * index + 1];
+		    data[2 * index + 1] = data[index];
+		    data[index] = temp;
+		    pushdown(2 * index + 1);
+		}
+	    }else if(!isMax && (data[2 * index + 2].compareTo(data[index]) < 0 || data[2 * index + 1].compareTo(data[index]) < 0)){
+		if(2*index + 2 < size){
+		    if(data[2 * index + 2].compareTo(data[2*index + 1]) < 0){
+			T temp = data[2 * index + 2];
+			data[2 * index + 2] = data[index];
+			data[index] = temp;
+			pushdown(2 * index + 2);
+		    }else {
+			T temp = data[2 * index + 1];
+			data[2 * index + 1] = data[index];
+			data[index] = temp;
+			pushdown(2 * index + 1);
+		    }
+		}else if(2*index + 2 < size){
+		    T temp = data[2 * index + 1];
 		    data[2 * index + 1] = data[index];
 		    data[index] = temp;
 		    pushdown(2 * index + 1);
@@ -106,28 +152,52 @@ public class MyHeap{
 	    }
 	}
     }
+    */
     public String toString(){
-	return Arrays.toString(data);
+	String ret = "";
+	int nodesPassed = 0;
+	for(int numNodes = 1; numNodes < size; numNodes*=2){
+	    for(int i = 0; i < numNodes && i + nodesPassed < size; i++){
+		ret += data[nodesPassed + i] + " ";
+	    }
+	    nodesPassed += numNodes;
+	    ret += "\n";
+	}
+
+	return ret;
 
     }
+
+
+    public T get(int index){
+	return data[index];
+    }
     public static void main(String[] args){
-	MyHeap m = new MyHeap();
-	String a = "";
+	MyHeap m = new MyHeap(false);
+	//String a = "";
 	Random r = new Random();
 	for(int i = 0; i < (int)Integer.parseInt(args[0]); i++){
-	    m.add("" + r.nextInt(10000));
-	    a+= i;
+	    m.add(Integer.valueOf(r.nextInt(10000)));
+	    //a+= i;
 	    //System.out.println(m);
 
 	}
+	boolean isgood = true;
+	for(int i = 0; i < m.size(); i++){
+	    if((2*i + 2 < m.size() && m.get(2 * i +2) != null) && (m.get(i).compareTo(m.get(2 * i + 1))<0 || m.get(i).compareTo(m.get(2 * i +2)) < 0)){
+		isgood = false;
+	    }
+	    System.out.println(isgood);
+	}
+	System.out.println(m);
 	for(int i = 0; i < (int)Integer.parseInt(args[0]); i++){
             System.out.println(m.remove());
-            a+= i;
+            //a+= i;
 
 	    }
 	String b = "1230";
 	String c = "123";
-	System.out.println(b.compareTo(a));
+	System.out.println(b.compareTo(c));
 
     }
 
